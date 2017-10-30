fd6ed0faf618473ec7d56ee27ee2b2ee2e7df5dc
Tyler Slabinski <tslabinski@slabity.net>
2017-10-30 13:16:44 -0400

    Move 'nixpkgs' changes to configuration.nix

--- conf/nixpkgs/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, expr, sys, ... }:
-
-with builtins; with pkgs.lib; {
-    # Allow proprietary software
-    nixpkgs.config.allowUnfree = true;
-
-    nix.binaryCaches = [ "https://cache.nixos.org" "https://nixcache.reflex-frp.org" ];
-    nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
-}
--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,22 @@
 { config, pkgs, ... }:
 
 {
-    imports = [
-          ./sys/hardware-configuration.nix
-          ./sys/accounts.nix
-          ./conf
-      ];
+  imports = [
+    ./sys/hardware-configuration.nix
+    ./sys/accounts.nix
+    ./conf
+  ];
 
-    _module.args.expr = import ./expr { inherit pkgs; };
-    _module.args.sys = import ./sys;
-    _module.args.buildVM = false;
+  _module.args.expr = import ./expr { inherit pkgs; };
+  _module.args.sys = import ./sys;
+  _module.args.buildVM = false;
+
+
+  nix = {
+    autoOptimiseStore = true;
+    daemonNiceLevel = 1;
+    daemonIONiceLevel = 1;
+  };
+
+  nixpkgs.config.allowUnfree = true;
 }
