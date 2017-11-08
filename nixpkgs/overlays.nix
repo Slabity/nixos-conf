04224988ff747b82e72e0322cf7614cd8bfda654
Tyler Slabinski <tslabinski@slabity.net>
2017-11-08 12:34:32 -0500

    Restructure to add nixpkgs

---
+++ nixpkgs/overlays.nix
@@ -0,0 +1,13 @@
+let
+    mozillaPkgsDir = (import <nixpkgs>{overlays=[];}).fetchFromGitHub {
+        owner = "mozilla";
+        repo = "nixpkgs-mozilla";
+        rev = "HEAD";
+#        sha256 = "1shz56l19kgk05p2xvhb7jg1whhfjix6njx1q4rvrc5p1lvyvizd";
+        fetchSubmodules = true;
+    };
+in
+[
+    (import "${mozillaPkgsDir}/rust-overlay.nix")
+    (import "${mozillaPkgsDir}/firefox-overlay.nix")
+]
