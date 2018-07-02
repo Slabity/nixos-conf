5a98693ea07bebfdfc44c9234ea21b79427c56ed
Tyler Slabinski <tslabinski@slabity.net>
2018-07-02 09:13:03 -0400

    Add hyannis

---
+++ systems/hyannis.nix
@@ -0,0 +1,50 @@
+{ config, ... }:
+{
+  imports = [
+    ../modules
+    ../hardware-configuration.nix
+  ];
+
+  foxos.system.name = "tslabinski-nixos";
+
+  foxos.hardware.efi = true;
+
+  foxos.hardware.cpu = {
+    type = "intel";
+    sockets = 1;
+    cores = 4;
+    threads = 1;
+  };
+
+  foxos.hardware.gpu.enable = true;
+  foxos.hardware.gpu.type.intel = true;
+  foxos.hardware.gpu.type.amd = false;
+
+  foxos.hardware.ethernet.enable = true;
+  foxos.hardware.wifi.enable = true;
+  foxos.hardware.bluetooth.enable = true;
+  foxos.hardware.audio.enable = true;
+  foxos.hardware.input.enable = true;
+  foxos.hardware.battery.enable = false;
+
+  foxos.profiles.workstation.enable = true;
+
+  foxos.mainUser.enable = true;
+  foxos.mainUser.name = "tslabinski";
+
+  time.timeZone = "America/New_York";
+
+  services.xserver.xrandrHeads = [
+    {
+      output = "HDMI-1";
+      primary = true;
+    }
+    {
+      output = "HDMI-3";
+      primary = false;
+      monitorConfig = ''
+        Option "PreferredMode" "3440x1440"
+      '';
+    }
+  ];
+}
